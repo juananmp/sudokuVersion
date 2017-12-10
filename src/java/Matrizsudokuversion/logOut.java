1a74e3c4c5ee36ed4654ed27d7b39340d712c613
janto <ja.merino@usp.ceu.es>
2017-12-10 22:58:24 +0100

    refactorizacion de codigo

--- src/java/Matrizsudokuversion/logOut.java
+++ src/java/Matrizsudokuversion/logOut.java
@@ -11,17 +11,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author janto
- */
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -30,26 +19,24 @@ import javax.servlet.http.HttpSession;
  */
 public class logOut extends HttpServlet {
 
-   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         HttpSession cliente = request.getSession();
         cliente.invalidate();
-           out.println("<link rel=\"stylesheet\" href=\"./resources/css/w3.css\">");
-         out.println("   <div class=\"loginbox\">");
+        out.println("<link rel=\"stylesheet\" href=\"./resources/css/w3.css\">");
+        out.println("   <div class=\"loginbox\">");
         out.println("<h1>Has hecho logOut</h1>");
         out.println("<br>");
         out.println("<a href=\"index.html\">Si quieres volver a logearte pulse aqui</a><br>");
         out.println("</div>");
         out.close();
-        
+
     }
 
-    
     /**
      * Returns a short description of the servlet.
      *
