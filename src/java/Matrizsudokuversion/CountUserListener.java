b7fee7f8da2b37771573f6e5a6cbd18bb101b783
janto <ja.merino@usp.ceu.es>
2017-12-10 22:57:00 +0100

    refactorizacion de codigo

--- src/java/Matrizsudokuversion/CountUserListener.java
+++ src/java/Matrizsudokuversion/CountUserListener.java
@@ -16,30 +16,27 @@ import javax.servlet.http.HttpSessionListener;
  */
 public class CountUserListener implements HttpSessionListener {
 
-   ServletContext ctx = null;
-    static int     totalUserCount = 0, currentUserCount = 0;
+    ServletContext ctx = null;
+    static int totalUserCount = 0, currentUserCount = 0;
 
-    public void sessionCreated( HttpSessionEvent httpSessionEvent )
-    {
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         System.out.println("sessionCreated method has been called in "
                 + this.getClass().getName());
 
-        
         totalUserCount++;
         currentUserCount++;
 
         ctx = httpSessionEvent.getSession().getServletContext();
-        
+
         ctx.setAttribute("totalusers", totalUserCount);
         ctx.setAttribute("currentusers", currentUserCount);
 
     }
 
-    public void sessionDestroyed( HttpSessionEvent httpSessionEvent )
-    {
-        
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+
         System.out.println("sessionDestroyed method has been called in "
-                + this.getClass().getName());       
+                + this.getClass().getName());
         currentUserCount--;
         ctx.setAttribute("currentusers", currentUserCount);
     }
