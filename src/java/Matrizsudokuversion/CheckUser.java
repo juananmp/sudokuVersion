65468e2a4406a6dfab840c9a7034535ccbe70159
janto <ja.merino@usp.ceu.es>
2017-12-24 19:27:24 +0100

    se han cambiado las variables de clase connection y statement a varaibles de método ya que ppodían podrucir concurrencia y tambien se liberan los recursos en esos mismos métodos (.close) También se han sustituido las paths de los html a xhtml y se ha añadido a nivel de registro, que la password debe ser mayor a 3 digitos y la doble verificación de password (password y password2) por si el usuario se equivocara al introducir la password

--- src/java/Matrizsudokuversion/CheckUser.java
+++ src/java/Matrizsudokuversion/CheckUser.java
@@ -21,69 +21,102 @@ import javax.sql.DataSource;
 
 /**
  *
+ *
+ *
  * @author janto
+ *
  */
 public class CheckUser extends HttpServlet {
 
     DataSource datasource;
-    Statement statement = null;
-    Connection connection = null;
+
+//    Statement statement = null;
+//
+//    Connection connection = null;
 
     //abre conexion con la Base de Datos
     @Override
+
     public void init() {
 
         try {
+
             InitialContext initialContext = new InitialContext();
             datasource = (DataSource) initialContext.lookup("jdbc/sudoku2");
+
         } catch (NamingException ex) {
             System.out.println(ex);
+
         }
+
     }
 
     /**
+     *
      * Handles the HTTP <code>GET</code> method.
      *
+     *
+     *
      * @param request servlet request
+     *
      * @param response servlet response
+     *
      * @throws ServletException if a servlet-specific error occurs
+     *
      * @throws IOException if an I/O error occurs
+     *
      */
     //Verifica si existe Usuario
     @Override
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+Statement statement = null;
 
+    Connection connection = null;
         ServletContext contexto = request.getServletContext();
 
         //Recuperamos usuario de la sesion
         HttpSession cliente = request.getSession();
+
         String user = (String) cliente.getAttribute("user");
 
         //verificamos si el usuario existe en la base de datos
         try {
 
             String query = null;
+
             query = "SELECT * FROM login WHERE user like '" + user + "'";
+
             ResultSet resulSet = null;
+
             connection = datasource.getConnection();
+
             statement = connection.createStatement();
+
             resulSet = statement.executeQuery(query);
 
             //si existe el usuario se indica error en password
             while (resulSet.next()) {
+
                 RequestDispatcher paginaError
-                        = contexto.getRequestDispatcher("/errorPassword.html");
+                        = contexto.getRequestDispatcher("/./faces/errorPassword.xhtml");
 
                 paginaError.forward(request, response);
+
             }
+
             // Si no existe el usuario, se indica error en el usuario
             RequestDispatcher paginaError
-                    = contexto.getRequestDispatcher("/errorUser.html");
+                    = contexto.getRequestDispatcher("/./faces/errorUser.xhtml");
 
             paginaError.forward(request, response);
+            
+            statement.close();
+            connection.close();
 
         } catch (SQLException ex) {
+
             System.out.println(ex);
 
         }
@@ -91,85 +124,135 @@ public class CheckUser extends HttpServlet {
     }
 
     /**
+     *
      * Handles the HTTP <code>POST</code> method.
      *
+     *
+     *
      * @param request servlet request
+     *
      * @param response servlet response
+     *
      * @throws ServletException if a servlet-specific error occurs
+     *
      * @throws IOException if an I/O error occurs
+     *
      */
     //Da de alta un nuevo usuario
     @Override
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+Statement statement = null;
 
+    Connection connection = null;
         String user = request.getParameter("user");
+
         String password = request.getParameter("password");
+        
+        String password2 = request.getParameter("password2");
+
         HttpSession cliente = request.getSession();
+
         cliente.setAttribute("user", user);
+
         cliente.setAttribute("password", password);
+
         ServletContext contexto = request.getServletContext();
 
         //Verifica si el usuario ya existe
         try {
 
             String query = null;
+
             query = "SELECT * FROM login WHERE user like '" + user + "'";
+
             ResultSet resulSet = null;
+
             connection = datasource.getConnection();
+
             statement = connection.createStatement();
+
             resulSet = statement.executeQuery(query);
 
             //Si el usuario ya existe, indica error
             while (resulSet.next()) {
 
                 RequestDispatcher paginaError
-                        = contexto.getRequestDispatcher("/esteUsuarioYaExiste.html");
+                        = contexto.getRequestDispatcher("/faces/esteUsuarioYaExiste.xhtml");
 
                 paginaError.forward(request, response);
+
             }
 
             //Si usuario no existe pero su longitud es inferior a 4 o no introduce la password, error
-            if (!(user.length() > 3) || password.equals("")) {
-                response.sendRedirect("registroIncorrecto.html");
+            if (!(user.length() > 3) || !(password.length() > 3) || !(password.equals(password2))) {
 
+                response.sendRedirect("/sudokuVersion/faces/registroIncorrecto.xhtml");
+
+                
                 //en caso contrario, encriptar password y dar de alta usuario
             } else {
 
                 response.sendRedirect("ServletHash");
-            }
 
+            }
+             statement.close();
+            connection.close();
         } catch (SQLException ex) {
+
             System.out.println(ex);
+
         }
 
     }
 
     //Cierra conexion con la Base de datos
     @Override
+
     public void destroy() {
+Statement statement = null;
+
+    Connection connection = null;
         try {
 
             statement.close();
+
         } catch (SQLException ex) {
+
             System.out.println(ex);
+
         } finally {
+
             try {
+
                 connection.close();
+
             } catch (SQLException ex) {
+
                 System.out.println(ex);
+
             }
+
         }
+
     }
 
     /**
+     *
      * Returns a short description of the servlet.
      *
+     *
+     *
      * @return a String containing servlet description
+     *
      */
     @Override
+
     public String getServletInfo() {
+
         return "Short description";
+
     }// </editor-fold>
 
 }
