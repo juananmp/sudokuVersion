9c62808c9297cfaf9026c15099cf21bc2a26175f
janto <ja.merino@usp.ceu.es>
2017-12-25 20:08:24 +0100

    como consecuencia de que en esta clase no se usaban los metodos de la servlet y además había una contradicción en el uso del método init, he decidido pasarla a una clase de Java normal, ya NO es una servlet

--- src/java/Matrizsudokuversion/Database.java
+++ src/java/Matrizsudokuversion/Database.java
@@ -1,42 +1,23 @@
 package Matrizsudokuversion;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
 /**
  *
  * @author janto
  */
-public class Database extends HttpServlet {
+public class Database {
 
     DataSource datasource;
-//    Statement statement = null;
-//    Connection connection = null;
 
     //Abre conexion con la base de datos
-    @Override
-    public void init() {
+    public void inicio() {
         try {
             InitialContext initialContext = new InitialContext();
 
@@ -47,36 +28,9 @@ public class Database extends HttpServlet {
 
     }
 
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-    }
-
     //Guarda el estado intermedio de un usuario para un numero de sudoku en la tabla intermedia
     public void guardar(String user, int[][] intermedia, int numSudoku) {
-        init();
+        inicio();
 
         int i = 0;
         int j = 0;
@@ -118,7 +72,7 @@ public class Database extends HttpServlet {
 
     //Borra un estado intermedio para un usuario y sudoku de la tabla intermedia
     public void borrar(String user, int numSudoku) {
-        init();
+        inicio();
 
         String query = null;
         query = "DELETE FROM intermedia WHERE user = '"
@@ -140,10 +94,10 @@ public class Database extends HttpServlet {
 
     //Lee un estadio intermedio de Sudoku de la tabla intermedia
     public int[][] plantillaIntermedia(String user, int numSudoku) {
-        init();
+        inicio();
         int[][] i = new int[9][9];
-         Statement statement = null;
-                Connection connection = null;
+        Statement statement = null;
+        Connection connection = null;
         try {
             String query = null;
             query = "SELECT * FROM intermedia WHERE user = '"
@@ -180,10 +134,10 @@ public class Database extends HttpServlet {
 
     //Lee el contenido de un sudoku de la tabla inicial o final, según se indique en la variable plantilla
     public int[][] plantilla(String plantilla, int numSudoku) {
-        init();
+        inicio();
         int[][] i = new int[9][9];
-         Statement statement = null;
-                Connection connection = null;
+        Statement statement = null;
+        Connection connection = null;
         try {
             String query = null;
             query = "SELECT * FROM " + plantilla + " where NumSudoku = " + numSudoku;
@@ -219,34 +173,5 @@ public class Database extends HttpServlet {
         return i;
 
     }
-
-    //Cierra conexion con la Base de datos
-    @Override
-    public void destroy() {
-         Statement statement = null;
-                Connection connection = null;
-        try {
-
-            statement.close();
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException ex) {
-                System.out.println(ex);
-            }
-        }
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
